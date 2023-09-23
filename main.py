8c23ff75eeef615e8b3be5af5e8ad2b07f913fd1
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-09-23 01:15:15 -0400

    improved MarketPlaceItem class and using regex got gun_name and wear

--- main.py
+++ main.py
@@ -21,20 +21,23 @@ class AppID(Enum):
     tf2 = "440"
     
 class MarketName(Enum):
-    ChromaCaseKey = "Chroma%202%20Case%20Key"
-    AWP = "AWP%20%7C%20Elite%20Build%20%28Minimal%20Wear"
-    case = "Revolution%20Case"
-    ak = "AK-47%20%7C%20Redline%20%28Field-Tested%29"
+    FT_AK_REDLINE = "AK-47%20%7C%20Redline%20%28Field-Tested%29"
+    ST_FT_AK_REDLINE = "StatTrak™%20AK-47%20%7C%20Redline%20%28Field-Tested%29"
 
 class MarketPlaceItem:
-    def __init__(self,name = str, lowest_price = str, volume = str, median_price = str):
+    def __init__(self,name = str, lowest_price = str, volume = str, median_price = str, wear = str, statTrak = bool):
         self.name = name
         self.volume = volume
         self.lowest_price = lowest_price
         self.median_price = median_price
+        self.wear = wear
+        self.statTrak = statTrak
     
     def print_self(self):
-        print(f"name: {self.name} || price: {self.lowest_price}")
+        if self.statTrak:
+            print(f"name: StatTrak {self.name} || wear: {self.wear} || price: {self.lowest_price}")
+        else:
+            print(f"name: {self.name} || wear: {self.wear} || price: {self.lowest_price}")
 
 class Currency(Enum):
     USD = "1"
@@ -47,13 +50,21 @@ def get_item_info(app_id: str,market_name: str, currency : str):
     
     if response.status_code == 200:
         data = json.loads(response.text)
-        marketplace_items[market_name] = MarketPlaceItem(market_name,data["lowest_price"],data["volume"],data["median_price"])
         
         if 'success' in data and data['success']:
             x = unquote(market_name)
-            print(x)
-            print(quote(x))
-            marketplace_items[market_name].print_self()
+            statTrak = False
+            if re.search("StatTrak", x):
+                x = x[10:]
+                statTrak = True
+                
+            gun_name = re.search("^(.*?)\s\(",x).group(1)          
+            wear = re.search("\((.*?)\)", x).group(1)
+            
+            if statTrak:
+                marketplace_items[f"{gun_name} StatTrak"] = MarketPlaceItem(gun_name,data["lowest_price"],data["volume"],data["median_price"],wear,True)
+            else:
+                marketplace_items[f"{gun_name}"] = MarketPlaceItem(gun_name,data["lowest_price"],data["volume"],data["median_price"],wear,False)
             
         else:
             print ("Failed to fetch data")
@@ -61,4 +72,9 @@ def get_item_info(app_id: str,market_name: str, currency : str):
     else:
         print(f"HTTP Error {response.status_code}")
 
-get_item_info(AppID.csgo.value,MarketName.ak.value,Currency.CAD.value)
+get_item_info(AppID.csgo.value,MarketName.ST_FT_AK_REDLINE.value,Currency.CAD.value)
+get_item_info(AppID.csgo.value,MarketName.FT_AK_REDLINE.value,Currency.CAD.value)
+
+
+for key, value in marketplace_items.items():
+    value.print_self()
