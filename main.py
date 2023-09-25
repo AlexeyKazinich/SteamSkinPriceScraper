ab016d847387484c1133fa0e1a0627f6e0e7003b
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-09-25 01:01:33 -0400

    added some code improvements, but requires refactoring

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ class MarketName(Enum):
     ST_FT_AK_REDLINE = "StatTrak™%20AK-47%20%7C%20Redline%20%28Field-Tested%29"
     AK_HEADSHOT = "AK-47%20%7C%20Head%20Shot%20%28Field-Tested%29"
     AK_LEETMUSEO = "AK-47%20%7C%20Leet%20Museo%20%28Factory%20New%29"
-
+    
+    
+    
 class MarketPlaceItem:
     def __init__(self,name = str, lowest_price = str, volume = str, median_price = str, wear = str, statTrak = bool):
         self.name = name
@@ -47,6 +49,24 @@ class MarketPlaceItem:
             print(f"name: StatTrak {self.name} || wear: {self.wear} || price: {self.lowest_price}")
         else:
             print(f"name: {self.name} || wear: {self.wear} || price: {self.lowest_price}")
+
+class MyItem:
+    def __init__(self, paid_price: float, market_name: str, quanitity: int = 1):
+        self.paid_price = paid_price
+        self.current_market_value = 0
+        self.market_name = market_name
+        self.quantity = quanitity #how many of this item do you have
+        
+        self.marketplaceitem = None
+    
+    def update_price(self):
+        temp = get_item_info("730",self.market_name,"20")
+        self.current_market_value = temp["lowest_price"]
+        self.marketplaceitem = MarketPlaceItem(temp["name"],temp["lowest_price"],temp["volume"],temp["median_price"],temp["wear"],temp["stattrak"])
+    
+    def print(self):
+        print(f"I paid: CDN$ {self.paid_price} || item costs: {self.current_market_value}")
+
 
 class Currency(Enum):
     USD = "1"
@@ -70,10 +90,16 @@ def get_item_info(app_id: str,market_name: str, currency : str):
             gun_name = re.search("^(.*?)\s\(",x).group(1)          
             wear = re.search("\((.*?)\)", x).group(1)
             
-            if statTrak:
-                marketplace_items[f"{gun_name} StatTrak"] = MarketPlaceItem(gun_name,data["lowest_price"],data["volume"],data["median_price"],wear,True)
-            else:
-                marketplace_items[f"{gun_name}"] = MarketPlaceItem(gun_name,data["lowest_price"],data["volume"],data["median_price"],wear,False)
+            return{
+                "name" : gun_name,
+                "lowest_price" : data["lowest_price"],
+                "median_price": data["median_price"],
+                "volume": data["volume"],
+                "wear": wear,
+                "gun_name": gun_name,
+                "stattrak": statTrak
+                
+            }
             
         else:
             print ("Failed to fetch data")
@@ -96,10 +122,25 @@ def write_to_excel():
     df.to_excel('output.xlsx', index=False, engine='openpyxl')
 
 
+
+
+my_items = {
+    "ak_blue_laminate" : MyItem(31.20,"StatTrak™%20AK-47%20%7C%20Blue%20Laminate%20(Minimal%20Wear)"),
+    "m4a4_living_color" : MyItem(24.96,"StatTrak™%20M4A4%20%7C%20In%20Living%20Color%20(Field-Tested)"),
+    "ak_elite_build" : MyItem(6.58,"StatTrak™%20AK-47%20%7C%20Elite%20Build%20(Well-Worn)"),
+    "ak_leet_museo" : MyItem(0,"StatTrak™%20AK-47%20%7C%20Leet%20Museo%20(Field-Tested)"),
+}
+
+
 if __name__ == '__main__':
-    get_item_info(AppID.csgo.value, MarketName.ST_FT_AK_REDLINE.value, Currency.CAD.value)
-    get_item_info(AppID.csgo.value, MarketName.FT_AK_REDLINE.value, Currency.CAD.value)
-    get_item_info(AppID.csgo.value, MarketName.AK_HEADSHOT.value, Currency.CAD.value)
-    get_item_info(AppID.csgo.value, MarketName.AK_LEETMUSEO.value, Currency.CAD.value)
+    for key, value in my_items.items():
+        value.update_price()
+        value.print()
+    # get_item_info(AppID.csgo.value, MarketName.ST_FT_AK_REDLINE.value, Currency.CAD.value)
+    # get_item_info(AppID.csgo.value, MarketName.FT_AK_REDLINE.value, Currency.CAD.value)
+    # get_item_info(AppID.csgo.value, MarketName.AK_HEADSHOT.value, Currency.CAD.value)
+    # get_item_info(AppID.csgo.value, MarketName.AK_LEETMUSEO.value, Currency.CAD.value)
     
-    write_to_excel()
+    # write_to_excel()
+    
+    
